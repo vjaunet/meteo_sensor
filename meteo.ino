9aa27193ca2595324215f7433f2e3f9f5b663fbf
Lem Pie <lem@pie.com>
2016-08-02 23:46:10 +0200

    clean up remove wifi

--- meteo.ino
+++ meteo.ino
@@ -16,80 +16,6 @@
 
 #include "meteo_sensor.h"
 #include <LowPower.h>
-#include <SoftwareSerial.h>
-#include <SparkFunESP8266WiFi.h>
-
-//Define the web server and the AccessPoint
-#define SSID "ESP8266_01"
-#define PASSWORD "1234567890"
-const String htmlHeader = "HTTP/1.1 200 OK\r\n"
-  "Content-Type: text/html\r\n"
-  "Connection: close\r\n\r\n"
-  "<!DOCTYPE HTML>\r\n"
-  "<html>\r\n";
-ESP8266Server server = ESP8266Server(80);
-void serve_webpage()
-{
-  // available() is an ESP8266Server function which will
-  // return an ESP8266Client object for printing and reading.
-  // available() has one parameter -- a timeout value. This
-  // is the number of milliseconds the function waits,
-  // checking for a connection.
-  ESP8266Client client = server.available(500);
-
-  if (client)
-    {
-      Serial.println(F("Client Connected!"));
-      // an http request ends with a blank line
-      boolean currentLineIsBlank = true;
-      while (client.connected())
-	{
-	  if (client.available())
-	    {
-	      char c = client.read();
-	      // if you've gotten to the end of the line (received a newline
-	      // character) and the line is blank, the http request has ended,
-	      // so you can send a reply
-	      if (c == '\n' && currentLineIsBlank)
-		{
-		  Serial.println(F("Sending HTML page"));
-		  // send a standard http response header:
-		  client.print(htmlHeader);
-		  String htmlBody;
-		  // output the value of each analog input pin
-		  for (int a = 0; a < 6; a++)
-		    {
-		      htmlBody += "A";
-		      htmlBody += String(a);
-		      htmlBody += ": ";
-		      htmlBody += String(analogRead(a));
-		      htmlBody += "<br>\n";
-		    }
-		  htmlBody += "</html>\n";
-		  client.print(htmlBody);
-		  break;
-		}
-	      if (c == '\n')
-		{
-		  // you're starting a new line
-		  currentLineIsBlank = true;
-		}
-	      else if (c != '\r')
-		{
-		  // you've gotten a character on the current line
-		  currentLineIsBlank = false;
-		}
-	    }
-	}
-      // give the web browser time to receive the data
-      delay(1);
-
-      // close the connection:
-      client.stop();
-      Serial.println(F("Client disconnected"));
-    }
-
-}
 
 #define BTN 2
 #define MENU_MAX 2
@@ -144,55 +70,6 @@ void setup() {
 
   /* init the devices */
   meteo_device.initialize();
-
-  /* init the esp8266 */
-  // esp8266.begin() verifies that the ESP8266 is operational
-  // and sets it up for the rest of the sketch.
-  // It returns either true or false -- indicating whether
-  // communication was successul or not.
-  if ( !esp8266.begin() )
-    {
-      Serial.println(F("Error talking to ESP8266."));
-    }
-
-  //set the esp8266 to AP mode
-  //  The ESP8266 can be set to one of three modes:
-  //  1 - ESP8266_MODE_STA - Station only
-  //  2 - ESP8266_MODE_AP - Access point only
-  //  3 - ESP8266_MODE_STAAP - Station/AP combo
-  // Use esp8266.getMode() to check which mode it's in:
-  if (esp8266.setMode(ESP8266_MODE_AP) < 0)
-    {
-      Serial.println(F("Error setting mode."));
-    }
-
-  //Beware that one may need to specify IP address range and mask
-   //before attempting to get an adress from a dchp server...
-
-  if (esp8266.setIPrange("192.168.5.1","192.168.5.1","255.255.255.0") <= 0)
-    {
-      Serial.println(F("Failed setting IP range"));
-    }
-
-  //set AccessPoint name and password
-  if ( !esp8266.setAP(SSID,PASSWORD) )
-    {
-      Serial.println(F("Error setting AP"));
-    }
-
-  //enable esp8266 DHCP
-  if ( !esp8266.enableDHCP() )
-    {
-      Serial.println(F("Error enabling DHCP"));
-    }
-
-  // begin initializes a ESP8266Server object. It will
-  // start a server on the port specified in the object's
-  // constructor (in global area)
-  server.begin();
-  Serial.print(F("Server started! Go to "));
-  Serial.println(esp8266.localIP());
-  Serial.println();
 
 }
 
